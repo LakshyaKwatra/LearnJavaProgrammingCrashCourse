bda721f9cffc5ee328a7609f223c830bce78fb76
Lakshya Kwatra <lkwatra2@gmail.com>
2021-07-14 19:40:26 +0530

    make SimpleMenu interactive

--- SimpleMenu/src/lakshya/learnjava/Main.java
+++ SimpleMenu/src/lakshya/learnjava/Main.java
@@ -1,24 +1,42 @@
 package lakshya.learnjava;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        String response = "y";
+        ArrayList<String> menu = new ArrayList<>(List.of("Cappuccino",
+                "Latte",
+                "Americano",
+                "Mocha",
+                "Machiato",
+                "Espresso"));
 
-        System.out.println("Please Choose Your Coffee");
-        System.out.println("1. Cappuccino");
-        System.out.println("2. Latte");
-        System.out.println("3. Americano");
-        System.out.println("4. Mocha");
-        System.out.println("5. Machiato");
-        System.out.println("6. Espresso");
-        System.out.println("7. Quit");
+        while(response.equals("y") || response.equals("Y")) {
+            System.out.println("Please Choose Your Coffee");
+            for(int i = 1; i <= menu.size(); i++){
+                System.out.printf("%d: %s\n", i, menu.get(i - 1));
+            }
+            System.out.println("Q: Quit");
 
-        String userChoice = scanner.nextLine();
-        System.out.println("You chose: " + userChoice);
-
+            String userChoice = scanner.nextLine();
+            try {
+                if (!userChoice.equals("q") && !userChoice.equals("Q")) {
+                    System.out.println("Here's your " + menu.get(Integer.parseInt(userChoice) - 1) + "!");
+                } else {
+                    System.out.println("Good Bye!");
+                    break;
+                }
+            } catch (Exception e){
+                System.out.println(e);
+            }
+            System.out.printf("Do you want more coffee? (y/n): ");
+            response = scanner.nextLine();
+        }
         scanner.close();
     }
 }
